1f18d4497e00ce2bebe2bb138dc5b18ebfcec8cf
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_mesosphere_dcos/centos.py
+++ offregister_mesosphere_dcos/centos.py
@@ -16,52 +16,70 @@ from yaml import load
 
 
 def housekeeping0(*args, **kwargs):
-    if sudo('docker ps', warn_only=True, quiet=True).failed:
-        raise EnvironmentError('Expected Docker. Include offregister-docker in your config.')
+    if sudo("docker ps", warn_only=True, quiet=True).failed:
+        raise EnvironmentError(
+            "Expected Docker. Include offregister-docker in your config."
+        )
 
-    disable_service('firewalld')
+    disable_service("firewalld")
 
-    sudo('mkdir -p /var/lib/dcos /var/lib/mesos')
-    append('/etc/sudoers', '%wheel ALL=(ALL) NOPASSWD: ALL', use_sudo=True)
+    sudo("mkdir -p /var/lib/dcos /var/lib/mesos")
+    append("/etc/sudoers", "%wheel ALL=(ALL) NOPASSWD: ALL", use_sudo=True)
 
-    if not cmd_avail('timedatectl') and run('timedatectl', quiet=True, warn_only=True):
-        raise EnvironmentError('Expected NTP to be enabled.')
+    if not cmd_avail("timedatectl") and run("timedatectl", quiet=True, warn_only=True):
+        raise EnvironmentError("Expected NTP to be enabled.")
 
-    yum_depends('tar', 'xz', 'unzip', 'curl', 'ipset')
+    yum_depends("tar", "xz", "unzip", "curl", "ipset")
 
-    append('/etc/environment', 'LANG={enc}\nLC_ALL={enc}'.format(enc='en_US.utf-8'), use_sudo=True)
+    append(
+        "/etc/environment",
+        "LANG={enc}\nLC_ALL={enc}".format(enc="en_US.utf-8"),
+        use_sudo=True,
+    )
 
     # <Cluster node>
-    if sudo("grep -F 'SELINUX=permissive' /etc/selinux/config", warn_only=True, quiet=True).failed:
-        sudo('sed -i s/SELINUX=enforcing/SELINUX=permissive/g /etc/selinux/config')
+    if sudo(
+        "grep -F 'SELINUX=permissive' /etc/selinux/config", warn_only=True, quiet=True
+    ).failed:
+        sudo("sed -i s/SELINUX=enforcing/SELINUX=permissive/g /etc/selinux/config")
         did_something = False
-        for group in ('nogroup', 'docker'):
-            if sudo('grep -Fq {group} /etc/group'.format(group=group)):
-                sudo('groupadd {group}'.format(group=group))
+        for group in ("nogroup", "docker"):
+            if sudo("grep -Fq {group} /etc/group".format(group=group)):
+                sudo("groupadd {group}".format(group=group))
                 did_something = True
         if did_something:
-            sudo('reboot')
+            sudo("reboot")
     # </Cluster node>
 
-    run('mkdir -p ~/Downloads')
-    with cd('~/Downloads'):
-        sh = 'dcos_generate_config.sh'
+    run("mkdir -p ~/Downloads")
+    with cd("~/Downloads"):
+        sh = "dcos_generate_config.sh"
         if not exists(sh):
-            run('curl -L https://downloads.dcos.io/dcos/stable/{sh} -o {sh}'.format(sh=sh))
+            run(
+                "curl -L https://downloads.dcos.io/dcos/stable/{sh} -o {sh}".format(
+                    sh=sh
+                )
+            )
 
 
 def configure1(*args, **kwargs):
-    config_join = partial(path.join,
-                          path.join(path.dirname(
-                              resource_filename(modules[__name__].__package__, '__init__.py')), '_config'))
+    config_join = partial(
+        path.join,
+        path.join(
+            path.dirname(
+                resource_filename(modules[__name__].__package__, "__init__.py")
+            ),
+            "_config",
+        ),
+    )
 
-    run('mkdir -p ~/genconf')
+    run("mkdir -p ~/genconf")
 
-    with open(config_join('config.yaml')) as f:
+    with open(config_join("config.yaml")) as f:
         d = load(f)
-        d['agent_list'] = []
+        d["agent_list"] = []
         pp(d)
 
-    run('touch ~/genconf/ipdetect')
-    with open(config_join('ipdetect.bash')) as f:
-        put(f, '~/genconf/ipdetect')
+    run("touch ~/genconf/ipdetect")
+    with open(config_join("ipdetect.bash")) as f:
+        put(f, "~/genconf/ipdetect")
